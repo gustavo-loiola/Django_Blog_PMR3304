a7f1988b81bd4835c02f6f80a072853c015fc1f9
Gustavo <gustavoloioladossantos@gmail.com>
2024-11-16 17:41:38 -0300

    atualizar urls.py para lidar com classes genericas

--- blogdinamico/noticias/urls.py
+++ blogdinamico/noticias/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'noticias'
 urlpatterns = [
-    path('', views.lista_noticias, name='listar'),  # Listar todas as notícias
-    path('<int:noticia_id>/', views.detalhe, name='detalhe'),  # Exibir detalhes de uma notícia
-    path('busca/', views.busca_noticias, name='buscar'),  # Buscar notícias
-    path('criar/', views.cria_noticia, name='criar'),  # Criar uma nova notícia
-    path('atualizar/<int:noticia_id>/', views.atualizar_noticia, name='atualizar'),  # Atualizar uma notícia
-    path('excluir/<int:noticia_id>/', views.excluir_noticia, name='excluir'),  # Excluir uma notícia com confirmação
+    path('', views.ListaNoticiasView.as_view(), name='listar'),
+    path('<int:pk>/', views.NoticiaDetalheView.as_view(), name='detalhe'),
+    path('busca/', views.busca_noticias, name='buscar'),
+    path('criar/', views.CriarNoticiaView.as_view(), name='criar'),
+    path('atualizar/<int:pk>/', views.AtualizarNoticiaView.as_view(), name='atualizar'),
+    path('excluir/<int:pk>/', views.ExcluirNoticiaView.as_view(), name='excluir'),
 ]
