10506d55c4dae58b52d6c4e9d4d3322d545b4a6b
Gustavo <gustavoloioladossantos@gmail.com>
2024-11-17 18:06:52 -0300

    implementar sqlite de desenvolvimento para producao

--- blogdinamico/settings.py
+++ blogdinamico/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,16 +80,17 @@ WSGI_APPLICATION = 'blogdinamico.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-if os.environ.get("DATABASE_URL"):
-    DATABASES["default"] = dj_database_url.config(
-        conn_max_age=600,
-    )
+# Se você quiser alternar entre SQLite e outro banco em produção, mantenha o código comentado para referência:
+# if os.environ.get("DATABASE_URL"):
+#     DATABASES["default"] = dj_database_url.config(
+#         conn_max_age=600,
+#     )
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
